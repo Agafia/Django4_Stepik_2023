ba4c5ba06f34b286c53a8f4b2ecb7dc9aad755f6
Alena_notebook <alenasemenihina@gmail.com>
2025-11-22 14:23:01 +0500

    4.7 Связь one-to-one (один к одному)

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,6 +1,5 @@
 # women/urls.py
-
-from django.urls import path, re_path, register_converter
+from django.urls import path, register_converter
 from . import views
 from . import converters
 
@@ -13,5 +12,6 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('login/', views.login, name='login'),
     path('post/<slug:post_slug>/', views.show_post, name='post'),
-    path('category/<int:cat_id>/', views.show_category, name='category'),
+    path('category/<slug:cat_slug>/', views.show_category, name='category'),
+    path('tag/<slug:tag_slug>/', views.show_tag_postlist, name='tag'),
 ]
